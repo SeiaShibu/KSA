2f1ff6b3feff3e89517680c9d54d5a66c8c89791
Seia Shibu <siyashibu.56@gmail.com>
2025-08-27 11:24:12 +0530

    render deployed

--- project/server/utils/app.ts
+++ project/server/utils/app.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // replace with your backend URL
+const API_URL = 'https://ksa-1qrk.onrender.com/api';
 
 export const authAPI = {
   login: async (email: string, password: string) => {
